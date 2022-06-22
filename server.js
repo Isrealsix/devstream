7dfd759c69863444d92a7c385a53004b08bd2b26
Isrealsix's MacBook Pro <isrealsix@gmail.com>
2022-06-22 13:11:51 +0700

    connected mongodb

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
+// Connect to DB
+connectDB();
 app.get('/', (req, res) => res.send('API RUNNING'))
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
